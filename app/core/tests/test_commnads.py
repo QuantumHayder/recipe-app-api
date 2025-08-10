e2ebc39082ab57f1f897fc14a146fc5f3401d746
Abd El-Rahman Hayder <lboodyde02@gmail.com>
2025-08-10 15:46:42 +0300

    fixing flake 8 error again2

--- app/core/tests/test_commnads.py
+++ app/core/tests/test_commnads.py
@@ -1,10 +1,10 @@
 """Test custome Django management commands"""
 
-# used to temporarily replace parts of your code with 
+# used to temporarily replace parts of your code with
 # mock objects during the test
 from unittest.mock import patch
 
-# Specific Postgres connection error, 
+# Specific Postgres connection error,
 # in case Postgres isn’t ready yet.
 from psycopg2 import OperationalError as Psycopg2Error
 
@@ -28,21 +28,21 @@ class CommandTests(SimpleTestCase):
     # To check if the db is immediatley available
     def test_wait_for_db_ready(self, patched_check):
         """Tets waiting for db if db is ready"""
-        # to somulate the db is ready and so 
+        # to somulate the db is ready and so
         # the test must be run exactly once
         patched_check.return_value = True
 
-        call_command('wait_for_db')  
+        call_command('wait_for_db')
         # to test that the command is setup correctly and callable
 
         patched_check.assert_called_once_with(databases=['default'])
 
-    # To check if the db is not availble immediately and we 
+    # To check if the db is not availble immediately and we
     # have to recheck several times
     @patch('time.sleep')
     def test_wait_for_db_delay(self, pathced_sleep, patched_check):
         """waiting for db when getting operational error"""
-        # call Psycopg2Error twice, OperationalError thrice, 
+        # call Psycopg2Error twice, OperationalError thrice,
         # and then finally return true(i.e. db is finally available)
         patched_check.side_effect = [Psycopg2Error] * 2 + \
             [OperationalError] * 3 + [True]

7b06f762aa8c06de2b7cba67a34afbd989b278af
Abd El-Rahman Hayder <lboodyde02@gmail.com>
2025-08-10 15:28:03 +0300

    added wait_for_db to sync db with django

--- app/app/tests.py
+++ app/app/tests.py
@@ -8,17 +8,19 @@ from app import calc
 class CalcTests(SimpleTestCase):
     """Test the calc module"""
 
-    def test_add_numbers(self): ## The methid name must start with the prefix: test_
+    def test_add_numbers(self):  # The method name must start
+        # with the prefix: test_
         """Test adding numbers together"""
-        res = calc.add(5,6)
+        res = calc.add(5, 6)
 
-        self.assertEqual(res,11)
+        self.assertEqual(res, 11)
 
     def test_subtract_numbers(self):
-        res = calc.subtract(10,15)
+        res = calc.subtract(10, 15)
 
-        self.assertEqual(res,5)
+        self.assertEqual(res, 5)
 
 
-## either group all the tests in 1 directory and this dir must conatin a __init__.py
-## or place tests.py module inside each app
+# either group all the tests in 1 directory and this 
+# dir must conatin a __init__.py
+# or place tests.py module inside each app

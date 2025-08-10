907c8ea96328d7640b8b5e19e64819a9245d9ae7
Abd El-Rahman Hayder <lboodyde02@gmail.com>
2025-08-10 22:09:20 +0300

    user model

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,32 @@
-from django.db import models  # noqa
+"""Database Models"""
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    "Manager fro users"
+
+    def create_user(self, email, password=None, **extra_fields):
+        "Create, save , and return a new user"
+        user = self.model(email=email, **extra_fields)
+        # **extra_fields is used in case we added new fields to the user
+        # model in the future, we don't modify the code and pass them
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+class User(AbstractBaseUser, PermissionsMixin):
+    "User in the system"
+    email = models.EmailField(max_length=2555, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default = True)
+    is_staff = models.BooleanField(default = False)
+    # To determine if user can log in to django admin
+
+    objects = UserManager()
+    
+    USERNAME_FIELD = 'email'
